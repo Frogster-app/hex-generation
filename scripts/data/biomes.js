d18c3bf925a58ae26873b87f39d7ff8f3e06e6b0
Reece Harris <reece.harris98@protonmail.com>
2022-08-20 10:40:34 +0100

    stage

--- scripts/data/biomes.js
+++ scripts/data/biomes.js
@@ -1,9 +1,10 @@
 function applyBiome(e) {
     // these thresholds will need tuning to match your generator
     if (e < 0.001) return ['#6071e5', 'Deep water'];
-    else if (e < 0.2) return ['#60cfe6', 'Water'];
+    else if (e < 0.1) return ['#60cfe6', 'Water'];
     else if (e < 0.55) return ['#60e682', 'Plains'];
-    else if (e < 0.55) return ['#60e682', 'Plains'];
+    else if (e < 0.75) return ['#34974D', 'forest'];
+    else if (e < 0.85) return ['#206F33', 'deep forest'];
 
     else return ['#fffafa', 'Undefined'];
 };
